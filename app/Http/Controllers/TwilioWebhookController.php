7f26895dfb42337cc90dcb39d9daec1b2562f456
oscar <oscarrosalessfg@gmail.com>
2025-11-11 13:19:36 +0000

    trying to fix Twilio: POST 419 error webhook

--- app/Http/Controllers/TwilioWebhookController.php
+++ app/Http/Controllers/TwilioWebhookController.php
@@ -8,6 +8,35 @@ use App\Models\Message;
 
 class TwilioWebhookController extends Controller
 {
+
+    public function handle(Request $request)
+    {
+        Log::info('Twilio webhook hit', $request->all());
+
+        $from = $request->input('From');
+        $body = $request->input('Body');
+
+        if (!$from || !$body) {
+            Log::warning('Twilio webhook missing data', ['from' => $from, 'body' => $body]);
+            return response('Bad Request', 400);
+        }
+
+        $chat = Chat::firstOrCreate(
+            ['whatsapp_number' => $from],
+            ['name' => 'Cliente ' . substr($from, -4)]
+        );
+
+        Message::create([
+            'chat_id' => $chat->id,
+            'sender' => 'client',
+            'message' => $body,
+        ]);
+
+        Log::info('Message saved from Twilio', ['from' => $from, 'body' => $body]);
+
+        return response('OK', 200);
+    }
+
     public function handle(Request $request)
     {
         $from = $request->input('From');
